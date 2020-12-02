6ff2815de2ec3657134963ea31265b2ea48c0ab9
Andrey Pividori <andrey.pividori@orange.fr>
2020-12-03 00:00:28 +0100

    Initialisation d'un modal pour créer un matériel

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -3,7 +3,14 @@
     <div class="columns">
       <div class="column is-1"></div>
       <listeMateriel class="column is-10" />
-      <div class="column is-1"></div>
+      <div class="column is-1">
+        <button id="show-modal" @click="showModal = true">Show Modal</button>
+        <!-- use the modal component, pass in the prop -->
+        <ajouterMateriel v-if="showModal" @close="showModal = false">
+          
+          <h3 slot="header">custom header</h3>
+        </ajouterMateriel>
+      </div>
     </div>
   </div>
 </template>
@@ -11,13 +18,15 @@
 <script>
 // @ is an alias to /src
 import listeMateriel from "@/components/Materiel/ListeMateriel"
+import ajouterMateriel from "@/components/Materiel/ajouterMateriel"
 
 export default {
   name: "Home",
-  components: {listeMateriel},
+  components: {listeMateriel, ajouterMateriel},
   data() {
     return {
       aMateriel: [],
+      showModal: false
     };
   },
   computed: {},
