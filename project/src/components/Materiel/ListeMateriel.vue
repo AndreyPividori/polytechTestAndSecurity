143e412d768357abf0a129191ef45fee2a0deaa9
Andrey Pividori <andrey.pividori@orange.fr>
2020-12-02 23:48:34 +0100

    Refactoring des composants, la liste du matériel apparait désormais dans le composant listeMateriel

---
+++ project/src/components/Materiel/ListeMateriel.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="liste-materiel">
+    <table
+      class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th><abbr title="Position">Index</abbr></th>
+          <th>Nom</th>
+          <th>Référence</th>
+          <th>N° Téléphone</th>
+          <th>Version</th>
+          <th>Photo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(materiel, index) in aMateriel" :key="index" :class="'row_' + index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ materiel.nom }}</td>
+          <td>{{ materiel.ref }}</td>
+          <td>{{ materiel.tel }}</td>
+          <td>{{ materiel.version }}</td>
+          <td>
+            <v-img :src="materiel.photo" alt=""
+                contain    
+                height="100px"
+                width="150px">
+            </v-img>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import * as utils from "@/store/utils.js";
+import firebase from "@/firebase.js";
+
+export default {
+  name: "ListeMateriel",
+  props: {
+  },
+  data() {
+    return {
+      aMateriel: [],
+    };
+  },
+  computed: {},
+  methods: {
+    createDoc: function() {
+      utils.createDoc("materiel", null);
+    },
+    getAllDocsFromCollection: function(collection) {
+      firebase.db
+        .collection(collection)
+        .get()
+        .then(querySnapshot => {
+          this.aMateriel = querySnapshot.docs.map(doc => doc.data());
+        });
+    }
+  },
+  mounted() {
+    this.getAllDocsFromCollection("materiel");
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,68 +1,23 @@
 <template>
   <div class="home">
-    <table
-      class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
-    >
-      <thead>
-        <tr>
-          <th><abbr title="Position">Index</abbr></th>
-          <th>Nom</th>
-          <th>Référence</th>
-          <th>N° Téléphone</th>
-          <th>Version</th>
-          <th>Photo</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(materiel, index) in aMateriel" :key="index">
-          <td>{{ index + 1 }}</td>
-          <td>{{ materiel.nom }}</td>
-          <td>{{ materiel.ref }}</td>
-          <td>{{ materiel.tel }}</td>
-          <td>{{ materiel.version }}</td>
-          <td>
-            <v-img :src="materiel.photo" alt=""
-                contain    
-                height="100px"
-                width="150px">
-            </v-img>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <listeMateriel />
     <button @click="createDoc()">Creer</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import * as utils from "@/store/utils.js";
-import firebase from "@/firebase.js";
+import listeMateriel from "@/components/Materiel/ListeMateriel"
 
 export default {
   name: "Home",
-  components: {},
+  components: {listeMateriel},
   data() {
     return {
-      aMateriel: []
+      aMateriel: [],
     };
   },
   computed: {},
-  methods: {
-    createDoc: function() {
-      utils.createDoc("materiel", null);
-    },
-    getAllDocsFromCollection: function(collection) {
-      firebase.db
-        .collection(collection)
-        .get()
-        .then(querySnapshot => {
-          this.aMateriel = querySnapshot.docs.map(doc => doc.data());
-        });
-    }
-  },
-  mounted() {
-    this.getAllDocsFromCollection("materiel");
-  }
+  methods: {}
 };
 </script>
