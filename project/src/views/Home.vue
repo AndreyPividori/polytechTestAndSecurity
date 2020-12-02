9d3df5d4d9fd1db11e6eff07365e476ff21a80e2
Andrey Pividori <andrey.pividori@orange.fr>
2020-12-02 12:06:11 +0100

    Création d'une fonction getDoc pour récupérer les données depuis firestore

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -2,17 +2,50 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>{{doc}}</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import firebase from "@/firebase.js";
 
 export default {
   name: "Home",
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      doc : "doc"
+    }
+  },
+  computed: {
+  },
+  methods: {
+    // function writeUserData(userId, name, email, imageUrl) {
+    //   firebase
+    //     .database()
+    //     .ref("users/" + userId)
+    //     .set({
+    //       username: name,
+    //       email: email,
+    //       profile_picture: imageUrl
+    //     });
+    // }
+    getDoc: async function(){
+      let cityRef = firebase.db.collection('materiel').doc('K8MOGu5nVZ7XxcLTggN5');
+      let doc = await cityRef.get();
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        console.log('Document data:', doc.data());
+      }
+    }
+  },
+  mounted() {
+    this.getDoc();
   }
 };
 </script>
