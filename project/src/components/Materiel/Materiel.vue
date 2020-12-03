0e9f9c4991d36841f6619901608543b82c7ef4c3
Andrey Pividori <andrey.pividori@orange.fr>
2020-12-03 17:42:50 +0100

    Ajout des informations dans la page matériel

--- project/src/components/Materiel/Materiel.vue
+++ project/src/components/Materiel/Materiel.vue
@@ -1,5 +1,43 @@
 <template>
   <div>
+      <div class="columns">
+          <div class="column is-1"></div>
+          <div class="column is-4">
+              <div>
+                <a :href="oDatas.photo">
+                    <v-img
+                        :src="oDatas.photo"
+                        alt=""
+                    >
+                    </v-img>
+                </a>
+              </div>
+          </div>
+          <div class="column is-6">
+              <div class="has-text-left">
+                 <strong>Statut : </strong> 
+                    <span v-if="oDatas.available">Disponible ✅</span>
+                    <span v-else >Indisponible 🚫</span>
+              </div>
+              <div class="has-text-left">
+                 <strong>Téléphone : </strong> 
+                 <span>{{oDatas.nom}}</span>
+              </div>
+              <div class="has-text-left">
+                 <strong>Référence : </strong> 
+                 <span>{{oDatas.ref}}</span>
+              </div>
+              <div class="has-text-left">
+                 <strong>Commentaire : </strong> 
+                 <span>{{oDatas.comment}}</span>
+              </div>
+              <br>
+              <div class="has-text-left">
+                <button class="button"> 🗓️Réserver maintenant !🗓️</button>
+              </div>
+          </div>
+          <div class="column is-1"></div>
+      </div>
   </div>
 </template>
 
