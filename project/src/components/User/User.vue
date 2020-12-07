fa9fb5bcfa6fde9bd71c3f5d88948b23cf9f0908
Pauline <paulinerouillon12@gmail.com>
2020-12-07 17:59:38 +0100

    Ajout des propriétés sur user

--- project/src/components/User/User.vue
+++ project/src/components/User/User.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>coucou</div>
+  <div>{{id}}</div>
 </template>
 
 <script>
 export default {
-
+  name:"user",
+  props: {
+    id: {
+      type: String,
+      default: ""
+    },
+    oDatas: {
+      type: Object,
+      required: true
+    }
+  }
 }
 </script>
 
