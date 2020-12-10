b0c3915d16638b084473708507bf08b7612e097e
Andrey Pividori <andrey.pividori@orange.fr>
2020-12-10 17:32:10 +0100

    Ajout des vérifications des champs pour la création d'un utilisateur

--- project/src/components/User/ajouterUser.vue
+++ project/src/components/User/ajouterUser.vue
@@ -24,11 +24,14 @@
                     class="input"
                     :class="isNameCorrect ? '' : 'is-danger'"
                   />
+                  <div v-if="!isNameCorrect" class="error">
+                    {{nameError}}
+                  </div>
                 </div>
               </div>
 
               <div class="field has-text-left">
-                <label class="label">Prénom :</label>
+                <label class="label">Prénom* :</label>
                 <div class="control">
                   <input
                     v-model.trim="signupForm.forname"
@@ -38,41 +41,48 @@
                     id="new-user-forname"
                   :class="isForNameCorrect ? '' : 'is-danger'"
                   />
+                   <div v-if="!isForNameCorrect" class="error">
+                    {{fornameError}}
+                  </div>
                 </div>
               </div>
 
               <div class="field has-text-left">
-                <label class="label">Matricule :</label>
+                <label class="label">Matricule* :</label>
                 <div class="control">
                   <input
                     v-model.trim="signupForm.matricule"
                     class="input"
-                    type="integer"
-                    placeholder="e.g 30033"
+                    type="text"
+                    placeholder="e.g KT30033"
                     id="new-user-matricule"
                   :class="isMatriculeCorrect ? '' : 'is-danger'"
                   />
+                  <div v-if="!isMatriculeCorrect" class="error">
+                    {{matriculeError}}
+                  </div>
                 </div>
               </div>
 
               <div class="field has-text-left">
-                <label class="label">Role :</label>
+                <label class="label">Role* :</label>
                 <div class="control">
-                  <input
+                  <label for="Administrateur"><input
                     type="radio"
                     id="role-admin"
                     value="true"
                     v-model.trim="signupForm.role"
                   />
-                  <label for="Administrateur">Administrateur</label>
+                  Administrateur</label>
                   <br />
-                  <input
+                  <label for="Emprunteur"><input
                     type="radio"
                     id="role-other"
                     value="false"
                     v-model.trim="signupForm.role"
+                    checked
                   />
-                  <label for="Emprunteur">Emprunteur</label>
+                  Emprunteur</label>
                   <br />
                 </div>
               </div>
@@ -87,7 +97,11 @@
                     placeholder="toto@you.com"
                     id="new-user-email"
                     required
+                    :class="isEmailCorrect ?'' :'is-danger'"
                   />
+                   <div v-if="!isEmailCorrect" class="error">
+                    {{emailError}}
+                  </div>
                 </div>
               </div>
 
@@ -97,11 +111,15 @@
                   <input
                     v-model.trim="signupForm.password"
                     class="input"
-                    type="text"
+                    type="password"
                     placeholder="**************"
                     id="new-user-pw"
                     required
+                  :class="isPassWordCorrect ?'' :'is-danger'"
                   />
+                   <div v-if="!isPassWordCorrect" class="error">
+                    {{passwordError}}
+                  </div>
                 </div>
               </div>
             </form>
@@ -139,10 +157,15 @@ export default {
         password: ""
       },
       isNameCorrect: true,
+      nameError: "Le nom choisi doit comprendre entre 1 et 30 caractères alphanumériques.",
       isForNameCorrect: true,
+      fornameError: "Le prénom choisi doit comprendre entre 1 et 30 caractères alphanumériques.",
       isEmailCorrect: true,
+      emailError:"Le format de l'email choisi est incorrect.",
       isMatriculeCorrect: true,
+      matriculeError: "La matricule comprendre entre 1 et 7 caractères alphanumériques.",
       isPassWordCorrect: true,
+      passwordError: "Votre mot de passe doit contenir au moins 6 caractères."
     };
   },
   methods: {
@@ -153,7 +176,42 @@ export default {
       let dDoc = null
       let _this = this;
 
-      //TODO Checker si les champs sont bien remplis
+      let AlphaNumRegEx = new RegExp(
+        "^([a-zA-Z0-9\u0600-\u06FF\u0660-\u0669\u06F0-\u06F9 _.-]+)$"
+      );
+      let aNames = this.signupForm.name.match(AlphaNumRegEx);
+      let aForNames = this.signupForm.forname.match(AlphaNumRegEx);
+      let aMatricules = this.signupForm.matricule.match(AlphaNumRegEx);
+
+      if (this.signupForm.name.length > 0 && this.signupForm.name.length < 31 && aNames != null) {
+        this.isNameCorrect = true
+      }else {
+        this.isNameCorrect = false
+      }
+
+      if (this.signupForm.forname.length > 0 && this.signupForm.forname.length < 31 && aForNames != null) {
+        this.isForNameCorrect = true
+      }else {
+        this.isForNameCorrect = false
+      }
+
+      if (!this.ValidateEmail(this.signupForm.email)) {
+        this.isEmailCorrect = false;
+      } else {
+        this.isEmailCorrect = true;
+      }
+
+      if (this.signupForm.matricule.length > 0 && this.signupForm.matricule.length < 31 && aMatricules != null) {
+        this.isMatriculeCorrect = true
+      }else {
+        this.isMatriculeCorrect = false
+      }
+      
+      if (this.signupForm.password.length > 5) {
+        this.isPassWordCorrect = true
+      }else {
+        this.isPassWordCorrect = false
+      }
 
       firebase.db.collection("users").where("email", "==", this.signupForm.email).get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
@@ -190,9 +248,18 @@ export default {
         email: email,
         password: pw
       });
-    }
+    },
+    ValidateEmail(email) {
+      //RFC 2822 standard email validation
+      var mailformat = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      return email.match(mailformat) && email.match(mailformat)[0] == email;
+    },
   }
 };
 </script>
 
-<style></style>
+<style>
+.error {
+  color: red;
+}
+</style>
