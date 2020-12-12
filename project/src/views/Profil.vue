723a3804ed7bba1e7dd679f4b34477fc18e71650
Andrey Pividori <andrey.pividori@orange.fr>
2020-12-12 12:39:15 +0100

    Un utilisateur peut supprimer son propre compte dans la page profil

--- project/src/views/Profil.vue
+++ project/src/views/Profil.vue
@@ -1,11 +1,42 @@
 <template>
   <div>
     Mon profil
+    <button @click="deleteAccount()">Delete</button>
   </div>
 </template>
 
 <script>
-export default {};
+import firebase from "@/firebase.js";
+import LogInVue from './LogIn.vue';
+
+export default {
+  name: "Profil",
+  components: {},
+  data() {
+    return {}
+  },
+  computed: {
+  },
+  methods: {
+    deleteAccount() {
+      let userUid = firebase.auth.currentUser.uid
+      let _this = this
+
+      firebase.db.collection("users").doc(userUid).delete().then(function(){
+        firebase.auth.currentUser.delete().then(() => {
+          _this.$store.dispatch("logout")
+          _this.$router.push('/login')
+          alert("Votre compte a bien été supprimé")
+        }).catch(error => {
+          console.log(error);
+        })
+      }).catch(error =>{
+        console.log(error);
+      } )
+
+    }
+  }
+};
 </script>
 
 <style></style>
